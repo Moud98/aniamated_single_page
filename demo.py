75c1873e6b3657a3d78db2bfda8ecb61150e3a43
Jolama-code <jolama0001@gmail.com>
2025-05-30 19:46:09 +0300

    fix: added line in the python file

--- demo.py
+++ demo.py
@@ -3,6 +3,8 @@ print("hello word")
 def number(house):
  if(house==2):
    print("there are two houses")
+ elif(house==5):
+  print("there are five houses")
  else:
   print("there are not house")  
 
