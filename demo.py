f3b8014b824ecf631983fec44451cba3b1618579
Jolama-code <jolama0001@gmail.com>
2025-05-30 19:51:54 +0300

    fix: added a line
    Ref:#1

--- demo.py
+++ demo.py
@@ -5,6 +5,8 @@ def number(house):
    print("there are two houses")
  elif(house==5):
   print("there are five houses")
+ elif(house==6):
+  print("there are six houses")
  else:
   print("there are not house")  
 
